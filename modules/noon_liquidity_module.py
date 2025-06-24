cbca9853bdb6aef7898b694f66ff8b68b6a384f9
Hanz <hi@hanz.dev>
2025-06-24 15:59:10 +0700

    feat: Added get_apy

--- modules/noon_liquidity_module.py
+++ modules/noon_liquidity_module.py
@@ -1,8 +1,21 @@
+import math
 from templates.liquidity_module import LiquidityModule, Token
 from typing import Dict, Optional
 from decimal import Decimal
 
-class MyProtocolLiquidityModule(LiquidityModule):
+class NoonLiquidityModule(LiquidityModule):
+    def _convert_to_shares(
+        self,
+        pool_state: Dict,
+        assets: int
+    ) -> int:
+        total_supply = pool_state.get('totalSupply', 0)
+        total_assets = pool_state.get('totalAssets', 0)
+        decimal_offset = 0
+        offset = 10 ** decimal_offset
+        
+        return math.floor(assets * (total_supply + offset) / (total_assets + 1))
+
     def get_amount_out(
         self, 
         pool_state: Dict, 
@@ -27,13 +40,56 @@ class MyProtocolLiquidityModule(LiquidityModule):
 
     def get_apy(
 		self, 
-		pool_state: Dict, 
+		pool_state: Dict,
 		underlying_amount:int,
 		underlying_token:Token, 
 		pool_tokens: Dict[Token.address, Token]
     ) -> int:
-        # 1 USN/sUSN price disrepancy between `days` compounded everyday for 365 days
-        pass
+        # 1 sUSN/USN price disrepancy between `days` compounded everyday for 365 days
+        usn_address = underlying_token.address
+        asset_decimals = pool_tokens[usn_address].decimals
+        days = pool_state.get('days', 0)
+        # Previous state
+        previous_total_assets = pool_state.get('prevTotalAssets', 0)
+        previous_total_supply = pool_state.get('prevTotalSupply', 0)
+        previous_pool_state = {
+            'totalAssets': previous_total_assets,
+            'totalSupply': previous_total_supply
+        }
+        # Current state
+        current_pool_state = {
+            'totalAssets': pool_state.get('totalAssets', 0),
+            'totalSupply': pool_state.get('totalSupply', 0)
+        }
+
+        if days == 0:
+            return 0
+        
+        # Dillute the current state
+        current_pool_state['totalAssets'] += underlying_amount
+        current_pool_state['totalSupply'] += self._convert_to_shares(
+            current_pool_state,
+            underlying_amount
+        )
+
+        # Calculate price disrepancy
+        previous_price = self._convert_to_shares(
+            previous_pool_state,
+            underlying_amount
+        )
+
+        current_price = self._convert_to_shares(
+            current_pool_state,
+            underlying_amount
+        )
+
+        d_price = current_price - previous_price
+        daily_apy = d_price / days
+
+        compounded_apy = (1 + daily_apy) ** 365 - 1
+        apy_bps = compounded_apy * 10000
+
+        return int(apy_bps)
 
     def get_tvl(
         self, 
@@ -54,7 +110,7 @@ class MyProtocolLiquidityModule(LiquidityModule):
         #     usn: '0x0469d9d1dE0ee58fA1153ef00836B9BbCb84c0B6',
         #     susn: '0xB6a09d426861c63722Aa0b333a9cE5d5a9B04c4f',
         # }
-        
+
         usn_amount = pool_state.get('totalSupply', 0)
         usn_address = fixed_parameters.get('usn_address')
         price = pool_tokens[usn_address].reference_price
