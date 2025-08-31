25b7414aa9e9968852b412dd4810b1e91b115b38
Hanz <hi@hanz.dev>
2025-08-31 13:59:07 +0700

    feat: Added get_tvl

--- modules/brownfi_liquidity_module.py
+++ modules/brownfi_liquidity_module.py
@@ -5,7 +5,7 @@ from decimal import Decimal
 class BrownFiLiquidityModule(LiquidityModule):
     def get_amount_out(
         self, 
-        pool_states: Dict, 
+        pool_state: Dict, 
         fixed_parameters: Dict,
         input_token: Token, 
         output_token: Token,
@@ -25,10 +25,36 @@ class BrownFiLiquidityModule(LiquidityModule):
         # Implement logic to calculate required input amount given output amount
         pass
 
-    def get_apy(self, pool_state: Dict) -> Decimal:
+    def get_apy(
+		self, 
+		pool_state: Dict,
+		underlying_amount:int,
+		underlying_token:Token, 
+		pool_tokens: Dict[str, Token]
+    ) -> int:
         # Implement APY calculation logic
         pass
 
-    def get_tvl(self, pool_state: Dict, token: Optional[Token] = None) -> Decimal:
-        # Implement TVL calculation logic
-        pass
+    def get_tvl(
+        self, 
+        pool_state: Dict,
+        fixed_parameters: Dict,
+        pool_tokens: Dict[str, Token]
+    ) -> int:
+        token0_amount = int(pool_state.get('token0_balance'))
+        token1_amount = int(pool_state.get('token1_balance'))
+        token0_address = fixed_parameters.get('token0_address')
+        token1_address = fixed_parameters.get('token1_address')
+
+        rprice0 = pool_tokens[token0_address].reference_price
+        rprice1 = pool_tokens[token1_address].reference_price
+
+        tvl0 = token0_amount * rprice0
+        tvl0 /= 10 ** pool_tokens[token0_address].decimals
+        
+        tvl1 = token1_amount * rprice1
+        tvl1 /= 10 ** pool_tokens[token1_address].decimals
+
+        tvl = tvl0 + tvl1
+
+        return tvl
